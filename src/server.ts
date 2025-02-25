c4e47ca5f5445007a6dfe34ba7a5f3ebb42e73ed
Therealkosbruh <realkoslulz@gmail.com>
2025-02-25 15:23:25 +0300

    added swagger documentation and libs

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,9 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import { authMiddleware } from "./middleware/authMiddleware";
-// import { register, login, getMe } from "./controllers/userController"; 
-// import { upvotePost, removeUpvote } from "./controllers/upvoteController";
-
-// import { 
-//     createFeedbackPost, 
-//     getAllFeedbackPosts, 
-//     getFeedbackPostById, 
-//     updateFeedbackPost, 
-//     deleteFeedbackPost 
-//   } from "./controllers/feedbackController"
-
-// dotenv.config();
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(express.json());  
-
-// //User's routes
-// app.post("/register", register);
-// app.post("/login", login);
-// app.get("/me", authMiddleware, getMe);
-
-
-// //Post's routes
-// app.post("/feedback", authMiddleware, createFeedbackPost);
-// app.get("/feedback", getAllFeedbackPosts);
-// app.get("/feedback/:id", getFeedbackPostById);
-// app.put("/feedback/:id", authMiddleware, updateFeedbackPost);
-// app.delete("/feedback/:id", authMiddleware, deleteFeedbackPost);
-
-
-// //Voting routes
-// app.post("/upvote/:postId", authMiddleware, upvotePost);
-// app.delete("/upvote/:postId", authMiddleware, removeUpvote);
-
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
 import { authMiddleware } from "./middleware/authMiddleware";
 import { register, login, getMe } from "./controllers/userController"; 
 import { upvotePost, removeUpvote } from "./controllers/upvoteController";
@@ -62,6 +23,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());  
+
+// Swagger setup
+const swaggerDocument = YAML.load("./openapi.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // User routes
 app.post("/register", register);
@@ -85,4 +50,5 @@ app.delete("/upvote/:postId", authMiddleware, removeUpvote);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Swagger UI доступен по адресу: http://localhost:${port}/api-docs`);
 });
